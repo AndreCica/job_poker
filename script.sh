326b4c4fa1933671bc637dd591e05195fd9b22ce
Cong <ngaicongleong@gmail.com>
2026-03-14 17:59:18 +0000

    Removed script.sh
    Removed git init script

--- script.sh
+++
@@ -1,7 +0,0 @@
-echo "# job_poker" >> README.md
-git init
-git add README.md
-git commit -m "first commit"
-git branch -M main
-git remote add origin https://github.com/AndreCica/job_poker.git
-git push -u origin main
